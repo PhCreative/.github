cc4831d13828589a82d8e3ac4963da4498c24734
Aaron <aaron@ph-creative.com>
2022-12-16 11:37:56 +0000

    Create TakeApplicationOffline.ps1

---
+++ .github/actions/ph-web-deploy-test/Scripts/TakeApplicationOffline.ps1
@@ -0,0 +1,32 @@
+param
+ (
+   [string]$source,
+   [string]$recycleApp,
+   [string]$computerName,
+   [string]$username,
+   [string]$password,
+   [string]$paramFile,
+   [string]$fileName
+ )
+
+ $msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe";
+
+ $computerNameArgument = $computerName + '/MsDeploy.axd?site=' + $recycleApp
+ 
+ $directory = Split-Path -Path (Get-Location) -Parent
+ $baseName = (Get-Item $directory).BaseName
+ $contentroot = $directory + '\' + $baseName
+
+ $remoteArguments = "computerName='${computerNameArgument}',userName='${username}',password='${password}',authType='Basic',"
+ 
+  [string[]] $appOfflineArguments = 
+ "-verb:sync",
+ "-source:contentPath=$contentroot\app_offline.htm",
+ "-dest:contentPath=$recycleApp,$($remoteArguments)includeAcls='False'",
+ "-allowUntrusted",
+ "-enableRule:DoNotDeleteRule"
+ 
+ $deployAppOfflineCommand = """$msdeploy"" $appOfflineArguments"
+ Write-Host $deployAppOfflineCommand
+ $appOfflineResult = cmd.exe /c "$deployAppOfflineCommand"
+ Write-Host $appOfflineResult
